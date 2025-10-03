3988a57775976a553d60e6fc8b5488df44c349a8
Shiv Soni <sonishiv309@gmail.com>
2025-10-03 18:06:22 +0530

    completed login controller

--- server/controllers/auth/login_controller.js
+++ server/controllers/auth/login_controller.js
@@ -1,8 +1,17 @@
-const login_controller = (req,res)=>{
-    return res.json({
-        success : true,
-        message : 'This is login page'
-    })
+const login_model = require("../../models/auth/login_model");
+
+const login_controller = async(req,res)=>{
+    const {email, password} = req.body;
+    try{
+    const login_user = await login_model(email, password);
+    if(login_user){
+        return res.status(200).json({message : 'Login successfull', user : login_user});
+    }
+    }
+    catch(err){
+        const status = err.status_code || 500;
+        return res.status(status).json({message : err.message || 'Internal server error'});
+    }
 }
 
 module.exports = login_controller;

533d8fbc929ef0a1fb2f798b39d3c5497a5f4945
Shiv Soni <sonishiv309@gmail.com>
2025-10-06 22:38:38 +0530

    added jwt sign function

--- server/controllers/auth/login_controller.js
+++ server/controllers/auth/login_controller.js
@@ -1,10 +1,17 @@
 const login_model = require("../../models/auth/login_model");
+const jwt = require('jsonwebtoken');
 
 const login_controller = async(req,res)=>{
     const {email, password} = req.body;
     try{
     const login_user = await login_model(email, password);
     if(login_user){
+        const payload = {
+            user_id : login_user.user_id,
+            user_email : login_user.user_email
+        }
+        const jwt_token = jwt.sign(payload, process.env.JWT_SECRET_KEY, {expiresIn : '24h'});
+        login_user.user_token = jwt_token;
         return res.status(200).json({message : 'Login successfull', user : login_user});
     }
     }
